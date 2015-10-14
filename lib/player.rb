a359d78527cd3daf821d6653b526db730f35698b
Amy Hunter <amyrhunter@gmail.com>
2015-10-14 16:12:43 -0700

    Added tests in #new and #play

--- lib/player.rb
+++ lib/player.rb
@@ -1,31 +1,28 @@
 module Scrabble
 	class Player
+		attr_accessor :name, :plays
 		def initialize(name)
-			# Called when you use Player.new, 
+			# Called when you use Player.new,
 			# sets up an instance with the instance variable name assigned
-			
+
 			@name = name
 			# returns the @name instance variable
 
 			@plays = []
 			# Returns an Array of the words played by the player
 		end
-		
-		def plays
-			return words_played
-		end
 
 		def play(word)
 			# Adds the input word to the plays Array
 			# Returns false if player has already won
-			
+
 		end
 
 		def self.total_score
 			# Sums up and returns the score of the players words
 		end
 
-		def self.won? 
+		def self.won?
 			# If the player has over 100 points, returns true, otherwise returns 'false'
 		end
 
