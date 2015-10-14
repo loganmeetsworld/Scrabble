9b15a1a0f4b258ceae330e32700cb3e034671dd5
Logan McDonald <loganmcdona11@gmail.com>
2015-10-14 16:25:23 -0700

    updated the spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,30 +1,27 @@
 require "./lib/player.rb"
 
-describe Scrabble::Player do
-  before :each do
-    @player = Scrabble::Player.new("Norman")
-  end
-
-  describe "#new" do
-    it "returns a Player object" do
-      expect(@player).to be_an_instance_of Scrabble::Player
-    end
-    it "returns the Player name" do
-      expect(@player.name).to eq "Norman"
-    end
-    it "returns an array of words played by the player" do
-      expect(@player.plays.class).to be Array
-    end
-  end
-
+describe "Player" do
+  player = Scrabble::Player.new("Norman")
+  
   describe "playing Scrabble" do
-    @player.play("hello")
-    @player.play("eggplant")
-    @player.play("coffee")
+    player.play("hello")
+    player.play("eggplant")
+    player.play("coffee")
 
-    it "returns array of played words" do
-      expect(@player.plays).to eq ["hello", "eggplant", "coffee"]
+    context "initializing" do 
+      it "returns a Player object" do
+        expect(player).to be_an_instance_of Scrabble::Player
+      end
+      it "returns the Player name" do
+        expect(player.name).to eq "Norman"
+      end
+      it "returns an array of words played by the player" do
+        expect(player.plays.class).to be Array
+      end
+    end
+
+    it "adds word" do
+      expect(player.plays).to eq ["hello", "eggplant", "coffee"]
     end
   end
-
 end
