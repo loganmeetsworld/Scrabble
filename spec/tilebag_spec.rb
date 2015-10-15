77e11ccd35060898f068930e64c9848ba2a4d4b5
Logan McDonald <loganmcdona11@gmail.com>
2015-10-15 16:34:07 -0700

    Added the tilebag files

--- spec/tilebag_spec.rb
+++ spec/tilebag_spec.rb
@@ -1,2 +1,19 @@
 require "./lib/scrabble_master.rb"
 
+describe "TileBag" do
+  tilebag = Scrabble::TileBag.new
+
+  context "initializing" do
+    it "returns a tilebag object" do
+      expect(tilebag).to be_an_instance_of Scrabble::TileBag
+    end
+  end
+  describe "#draw_tiles(num)" do
+    it "returns random tiles" do
+      expect(draw_tiles(rand(7))).to not_eq draw_tiles(rand(7))
+    end
+    # it "removes tiles from default set" do
+    #   expect(drawn_tiles)
+    # end
+  end
+end
