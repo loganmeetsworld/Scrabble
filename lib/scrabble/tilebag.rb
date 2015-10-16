5672c4f238bdb5ef4f284daedb4a09d0fd7f5157
Logan McDonald <loganmcdona11@gmail.com>
2015-10-16 09:45:12 -0700

    Saving files

--- lib/scrabble/tilebag.rb
+++ lib/scrabble/tilebag.rb
@@ -3,20 +3,33 @@ module Scrabble
     attr_accessor :tiles
     def initialize
       @tiles = {
-        A: 9, B: 2, C: 2, D: 4, E: 12,
-        F: 2, G: 3, H: 2, I: 9, J: 1,
-        K: 1, L: 4, M: 2, N: 6, O: 8,
-        P: 2, Q: 1, R: 6, S: 4, T: 6,
-        U: 4, V: 2, W: 2, X: 1, Y: 2,
-        Z: 1
+        "A": 9, "B": 2, "C": 2, "D": 4, "E": 12,
+        "F": 2, "G": 3, "H": 2, "I": 9, "J": 1,
+        "K": 1, "L": 4, "M": 2, "N": 6, "O": 8,
+        "P": 2, "Q": 1, "R": 6, "S": 4, "T": 6,
+        "U": 4, "V": 2, "W": 2, "X": 1, "Y": 2,
+        "Z": 1
       }
     end
 
     def draw_tiles(num)
+      key_array = @tiles.keys
+
+      tiles_drawn = key_array.sample(num)
+
+      tiles_drawn.each do |letter|
+        @tiles[letter] -= 1
+      end
+
+      return tiles_drawn
     end
 
     def tiles_remaining
       #returns number of tiles left in bag
+
+      tiles.each do |key, value|
+        value += value 
+      end
     end
 
   end
--- spec/tilebag_spec.rb
+++ spec/tilebag_spec.rb
@@ -8,12 +8,12 @@ describe "TileBag" do
       expect(tilebag).to be_an_instance_of Scrabble::TileBag
     end
   end
-  describe "#draw_tiles(num)" do
-    it "returns random tiles" do
-      expect(draw_tiles(rand(7))).to not_eq draw_tiles(rand(7))
-    end
-    # it "removes tiles from default set" do
-    #   expect(drawn_tiles)
-    # end
+  
+  describe "#draw_tiles"
+	  it "returns random tiles" do
+	    expect(tilebag.draw_tiles(rand(7))).not_to eq tilebag.draw_tiles(rand(7))
+	  end
+  it "removes tiles from default set" do
+    expect(drawn_tiles)
   end
 end
