f2a1bad235dc1f1f5319d6e23cb6c7e95da7258f
Logan McDonald <loganmcdona11@gmail.com>
2015-10-16 13:43:09 -0700

    Added the dictionary file

--- lib/scrabble/dictionary.rb
+++ lib/scrabble/dictionary.rb
@@ -1,4 +1,11 @@
+require "ruby-dictionary" 
+
 module Scrabble
 	class Dictionary
+
+		attr_accessor :dictionary
+		def initiliaze 
+			@dictionary = Dictionary.from_file('./support/dictionary.txt', ' ')
+		end
 	end
 end
